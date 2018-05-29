37afb9512f2009edda2def169e8d43e95db32a32
AndrewMoiseev <Andrew.Moiseev@gmail.com>
2018-05-29 20:13:54 +0500

    Solved task 2 (ruby)

--- task2.rb
+++ task2.rb
@@ -7,3 +7,26 @@
 #Вывести -1, если такой сдвиг не существует.
 #На вход подается две строки длины a и b. 1 <= a, b <= 10000
 
+
+a = ARGV[0]
+b = ARGV[1]
+
+shift = -1
+arr_a = a.split("")
+arr_b = b.split("")
+
+i = 1
+
+begin
+        arr_a << arr_a.shift
+        if (arr_a == arr_b) then
+                shift = i
+        end
+        i += 1
+end until (shift > 0)||(i == arr_a.count)
+
+if (shift > arr_a.count/2)
+  shift =  arr_a.count - shift
+end
+
+puts shift
