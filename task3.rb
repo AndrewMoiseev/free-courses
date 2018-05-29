31323996787e18d65d0956fa47bff6f0456d077f
AndrewMoiseev <Andrew.Moiseev@gmail.com>
2018-05-29 18:02:50 +0500

    Solved task 3 (Ruby)

--- task3.rb
+++ task3.rb
@@ -4,3 +4,18 @@
 
 #Реализовать функцию расчета наибольшего общего делителя для чисел a и b.
 #На вход подается два числа a и b, разделенные пробелом. 1 <= a, b <= 10000
+
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+
+nod = [a,b].min
+
+nod+=1
+begin
+  nod-=1
+  adiv = a%nod
+  bdiv = b%nod
+end until ((adiv+bdiv) == 0)
+
+puts nod
+
