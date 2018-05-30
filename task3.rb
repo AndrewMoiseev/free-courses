775893c537a95cf0ad3ae8a6c96ac993b779f8a9
AndrewMoiseev <Andrew.Moiseev@gmail.com>
2018-05-30 17:40:02 +0500

    added function method_nod

--- task3.rb
+++ task3.rb
@@ -5,17 +5,19 @@
 #Реализовать функцию расчета наибольшего общего делителя для чисел a и b.
 #На вход подается два числа a и b, разделенные пробелом. 1 <= a, b <= 10000
 
-a = ARGV[0].to_i
-b = ARGV[1].to_i
+#a = ARGV[0].to_i
+#b = ARGV[1].to_i
 
-nod = [a,b].min
+def method_nod(a,b)
+	nod = [a,b].min
 
-nod+=1
-begin
-  nod-=1
-  adiv = a%nod
-  bdiv = b%nod
-end until ((adiv+bdiv) == 0)
+	nod+=1
+	begin
+	  nod-=1
+	  adiv = a%nod
+	  bdiv = b%nod
+	end until ((adiv+bdiv) == 0)
+	return nod
+end
 
-puts nod
-
+puts method_nod(ARGV[0].to_i,ARGV[1].to_i)
